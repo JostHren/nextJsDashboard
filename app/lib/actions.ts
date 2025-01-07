1f2697b38d11b9deccf5984b5c8ac36a34b8c131
Jost Hren <josthren@gmail.com>
2025-01-07 11:57:13 +0100

    fix(fe): fix lint issues

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -103,9 +103,7 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
-  export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
-    
+  export async function deleteInvoice(id: string) {    
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
